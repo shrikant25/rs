18b41c6f7630257e7d0aaf9a7d3c88b9ed64e77f
shrikant <shrikantwankhede24@gmail.com>
2022-11-06 22:51:14 +0530

    oops

--- fsver2/vdwrite_flags_to_disk.h
+++
@@ -1,7 +0,0 @@
-#ifndef _VD_WRITEFLAGS_TODISK_H
-#define _VD_WRITEFLAGS_TODISK_H
-
-#include "vddiskinfo.h"
-int write_flags_todisk(int ,unsigned long int *, DISKINFO);
-
-#endif
--- fsver2/vdwrite_to_buffer.h
+++
@@ -1,9 +0,0 @@
-#ifndef _WRT_BUF
-#define _WRT_BUF
-
-#ifndef _WRITE_TO_BUFFER
-#define _WRITE_TO_BUFFER
-void write_to_buffer(char *buffer, char *val, unsigned int num_of_bytes, unsigned int index);
-#endif
-
-#endif
