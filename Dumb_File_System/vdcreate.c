66d148123b428a1adbd72e015dfd7661a1d4e598
shrikant25 <shrikantwankhede24@gmail.com>
2022-12-13 23:13:34 +0530

    modified

--- Dumb_File_System/vdcreate.c
+++ Dumb_File_System/vdcreate.c
@@ -14,6 +14,9 @@
 
 #define METADATA_BLOCKS_RATIO 1/(VDQUAD * VDBYTESZ * VDKB)
 
+int createfile(DISKINFO);
+int create_metadata_blocks(int, DISKINFO, char *);
+
 int createfile(DISKINFO DSKINF){
 
    int pid;
@@ -40,11 +43,11 @@ int createfile(DISKINFO DSKINF){
 }
 
 
-int create_metadata_blocks(int fd, DISKINFO DSKINF){
+int create_metadata_blocks(int fd, DISKINFO DSKINF, char *buffer){
    
    unsigned int i;
    unsigned int start = 0;
-   char *new_buffer = malloc(sizeof(char) * DSKINF.blksz);
+
    unsigned int ttl_mtdblks_in_dskblk = DSKINF.blksz/sizeof(FL_METADATA);
    FL_METADATA flmtd;
 
@@ -53,18 +56,17 @@ int create_metadata_blocks(int fd, DISKINFO DSKINF){
    flmtd.flsz = -1;
    flmtd.isavailable = 1;
 
-   memset(new_buffer, '\0', DSKINF.blksz);
+   memset(buffer, 0, DSKINF.blksz);
 
    for(i = 0; i<ttl_mtdblks_in_dskblk; i++){
-      write_to_buffer(new_buffer, (char *)&flmtd, sizeof(FL_METADATA), sizeof(FL_METADATA)*i);
+      write_to_buffer(buffer, (char *)&flmtd, sizeof(FL_METADATA), sizeof(FL_METADATA)*i);
    }
 
    start = DSKINF.mtdta_blk_ofst;
    for(i = start; i<DSKINF.dsk_blk_for_mtdata+start ; i++){
-      vdwrite(fd, new_buffer, i, DSKINF.blksz);
+      vdwrite(fd, buffer, i, DSKINF.blksz);
    }
 
-   free(new_buffer);
    return 0;
    
 }
@@ -80,17 +82,25 @@ int main(int argc, char *argv[]){
    int i, j;
    int fd = 0;
    unsigned char *chptr = NULL;
-   unsigned char *buffer;
+   unsigned char *buffer = NULL;
    unsigned int preoccupied_blocks = 0;
-   unsigned int *preoccupied_blocks_list; 
-   unsigned long int *flags = NULL;
+   unsigned int occupblk_to_set = 0;
    DISKINFO DSKINF;
 
    DSKINF.dsksz = pow(2, atoi(argv[2]));
    DSKINF.blksz = pow(2, atoi(argv[3]));
    buffer = malloc(sizeof(char) * DSKINF.blksz);
+   if(buffer == NULL){
+      printf("In sufficient memory\n");
+      exit(EXIT_FAILURE);
+   }
 
    DSKINF.diskname = malloc(sizeof(char) * (strlen(argv[1]) + 1));
+   if(DSKINF.diskname == NULL){
+      printf("In sufficient memory\n");
+      exit(EXIT_FAILURE);
+   }
+
    strcpy(DSKINF.diskname, argv[1]);
    DSKINF.diskname[strlen(argv[1])] = '\0'; 
 
@@ -115,27 +125,28 @@ int main(int argc, char *argv[]){
    vdwrite(fd, buffer, 0, DSKINF.blksz);
    
    free(DSKINF.diskname);
-   free(buffer);
    
-   flags = malloc(sizeof(unsigned long int) * DSKINF.flags_arrsz);
-   memset(flags, 0xFF, DSKINF.flags_arrsz*VDQUAD);
-
    preoccupied_blocks = 1 + DSKINF.flgblkcnt + DSKINF.dsk_blk_for_mtdata;
-   preoccupied_blocks_list = malloc(sizeof(unsigned int) * preoccupied_blocks);
- 
-   for(i = 0; i<preoccupied_blocks; i++){
-      preoccupied_blocks_list[i] = i;
-   }
 
-   setbits(preoccupied_blocks_list, preoccupied_blocks, 0, flags);
-   free(preoccupied_blocks_list);
+   for(i = 1; i<=DSKINF.flgblkcnt; i++){
+	
+		memset(buffer, 0xFF, DSKINF.blksz);
 
-   write_flags_todisk(fd, flags, DSKINF);
-   free(flags);
-   
-   create_metadata_blocks(fd, DSKINF);
+      if(preoccupied_blocks > 0){
+         occupblk_to_set = preoccupied_blocks < (DSKINF.blksz * VDBYTESZ) ? preoccupied_blocks : (DSKINF.blksz * VDBYTESZ);
+         memset(buffer, 0x00, occupblk_to_set/VDBYTESZ);
+         preoccupied_blocks -= occupblk_to_set;
+         buffer[occupblk_to_set/VDBYTESZ] = ~((1<<(occupblk_to_set%VDBYTESZ))-1);
+      }
+
+		vdwrite(fd, buffer, i, DSKINF.blksz);
+
+	}
+
+   create_metadata_blocks(fd, DSKINF, buffer);   
+   free(buffer);  
+   close(fd);
    
    printf("Disk creation successfull\n");
-   close(fd);
    return 0;
 }
